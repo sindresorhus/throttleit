620639c08e595505b5d6f264f45aa9abd41f298a
Nathan Rajlich <nathan@tootallnate.net>
2013-03-26 14:22:24 -0700

    pimp out

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 
-module.exports = function(fn, ms){
-  var ok = true;
-  return function(){
-    if (!ok) return;
+/**
+ * Module exports.
+ */
 
-    // invoke
-    fn.apply(this, arguments);
+module.exports = throttle;
 
-    // timeout
-    ok = false;
-    setTimeout(function(){
-      ok = true;
-    }, ms);
-  }
-};
+/**
+ * Returns a new function that, when invoked, invokes `func` at most one time per
+ * `wait` milliseconds.
+ *
+ * @param {Function} func The `Function` instance to wrap.
+ * @param {Number} wait The minimum number of milliseconds that must elapse in between `func` invokations.
+ * @return {Function} A new function that wraps the `func` function passed in.
+ * @api public
+ */
+
+function throttle (func, wait) {
+  var rtn; // return value
+  var last = 0; // last invokation timestamp
+  return function throttled () {
+    var now = new Date().getTime();
+    var delta = now - last;
+    if (delta >= wait) {
+      rtn = func.apply(this, arguments);
+      last = now;
+    }
+    return rtn;
+  };
+}
