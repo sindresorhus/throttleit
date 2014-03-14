58f9df3acc8a6afec19ad7f5504e1ffd1bc5eea0
Calvin French-Owen <calvin@calv.info>
2014-03-14 16:29:25 -0700

    adding `max` field
    Adds a `max` field as the third argument so that more than one function
    call can happen within the millisecond time.

--- index.js
+++ index.js
@@ -6,25 +6,29 @@
 module.exports = throttle;
 
 /**
- * Returns a new function that, when invoked, invokes `func` at most one time per
+ * Returns a new function that, when invoked, invokes `func` at most `max` times per
  * `wait` milliseconds.
  *
  * @param {Function} func The `Function` instance to wrap.
- * @param {Number} wait The minimum number of milliseconds that must elapse in between `func` invokations.
+ * @param {Number} wait The minimum number of milliseconds that must elapse in between `max` `func` invocations.
+ * @param {Number} max The maximum number of times `func` may be invoked between `wait` milliseconds.
  * @return {Function} A new function that wraps the `func` function passed in.
  * @api public
  */
 
-function throttle (func, wait) {
+function throttle (func, wait, max) {
+  max = max || 1;
   var rtn; // return value
   var last = 0; // last invokation timestamp
+  var count = 0; // number of times invoked
   return function throttled () {
     var now = new Date().getTime();
     var delta = now - last;
-    if (delta >= wait) {
-      rtn = func.apply(this, arguments);
+    if (delta >= wait) { // reset
       last = now;
+      count = 0;
     }
+    if (count++ < max) rtn = func.apply(this, arguments);
     return rtn;
   };
 }
